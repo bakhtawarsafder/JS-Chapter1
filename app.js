7541c490c2887db379b5a4b295ca8d251f01b7a8
email <bakhtawarsafder1@gmail.com>
2026-02-21 14:09:43 +0500

    first commit

---
+++ app.js
@@ -0,0 +1,14 @@
+// question 1
+alert("hello salam ");
+
+// question 2
+alert("Error! please enter a valid password");
+
+// qustion 3
+alert("Welcome to JS Land \n Happy Coding!")
+
+// question 4
+alert("Welcome to JS Land")
+
+// question 5
+alert("Hello i can run JS through my web browser's console") 
