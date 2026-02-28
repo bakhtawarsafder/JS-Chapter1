3bd30582e02bd69b06c3266c2f67c9b16d8ec4c3
mahnoorshaikh608 <mahnoorshaikh608@gmail.com>
2026-02-28 14:09:38 +0500

    done

---
+++ chapter2/app.js
@@ -0,0 +1,44 @@
+// qustion1 
+let userName = "username";
+
+alert(userName)
+
+// quetion2
+
+let myName = "bakhtawarsafder";
+alert(myName)
+
+// question3
+
+let message = "hello world";
+ alert(message)
+
+// question 4
+ let name = "jhone doe ";
+ let age = 15;
+ let course = "certified mobile application development" ;
+
+ alert(name)
+ alert(age)
+ alert(course)
+
+//  question 5
+let food = "PIZZA \n PIZZ \n PIZ \n PI \n P";
+alert(food)
+
+// question 6
+let email = "bakhtawarsafder1@gmail.com";
+alert(email)
+
+// question 7
+ let  alphabet = "i am trying to book A smarter way to learn JavaScript" ;
+ alert(alphabet)
+
+
+// question 8
+let  desktop ="Yah1 i can write HTML content through javascript" ;
+
+document.write(desktop)
+//  question 9
+ let diagram ="“▬▬▬▬▬▬▬▬▬ஜ۩۞۩ஜ▬▬▬▬▬▬▬▬▬”" ;
+ alert(diagram)
---
+++ chapter3/app.js
@@ -0,0 +1,18 @@
+// question1
+let age = "i am 15 year old"
+alert(age)
+
+// question2
+let apply = "you have visited this site 14 time "
+alert(apply)
+
+// question3
+let birth ="my birth year is 2010 Data type of my  declared variable is number "
+
+document.write(birth)
+document.write(<br/>)
+document.write(data)
+// question4
+let  make = "John Doe ordered 5 T-shirt(s) on XYZ Clothing store"
+alert(make)
+
