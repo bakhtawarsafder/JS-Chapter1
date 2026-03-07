9234117c297e2a237389019b285ab337de7cd0b5
subreenaraheelshaykh-lab <subreenaraheelshaykh@gmail.com>
2026-03-07 13:58:31 +0500

    done

---
+++ chapter5/app.js
@@ -0,0 +1,73 @@
+// question1
+
+let num1 = +prompt( "Enter your First Number");
+let num2 = +prompt ( "Enter your Second Number");
+
+let sum = num1 + num2;
+document.write(`Sum of ${num1} and ${num2} is ${sum}`);
+document.write("<br />");
+
+
+// question2
+// b. Show the value of variable in your browser like “Value
+// after variable declaration is: ??”.
+let num = ("three")
+
+document.write(`Value after variable declaration is ${num}}`);
+document.write("<br />");
+
+// question3
+
+
+
+
+// question4
+let cost = ("600")
+let total =("5")
+ let multiply =cost * total 
+document.write(`Total cost to buy ${total} tickets to a movie is ${multiply}`);
+document.write("<br />");
+
+
+
+
+
+// question5
+
+let count = 1;
+let tableNum = 4;
+
+document.write("Table of 4");
+
+document.write("<br />");
+document.write(`${tableNum} * ${count} = ${tableNum}`);
+document.write("<br />");
+count++;
+
+document.write(`${tableNum}  * ${count} = ${tableNum * count}`);
+document.write("<br />");
+count++; 
+document.write(`${tableNum} * ${count} = ${tableNum * count}`);
+document.write("<br />");
+count++; 
+document.write(`${tableNum} * ${count} = ${tableNum * count}`);
+document.write("<br />");
+count++;
+ document.write(`${tableNum} * ${count} = ${tableNum * count}`);
+document.write("<br />");
+count++;
+ document.write(`${tableNum} * ${count} = ${tableNum * count}`);
+document.write("<br />");
+count++;
+ document.write(`${tableNum} * ${count} = ${tableNum * count}`);
+document.write("<br />");
+count++; 
+ document.write(`${tableNum} * ${count} = ${tableNum * count}`);
+document.write("<br />");
+count++; 
+ document.write(`${tableNum} * ${count} = ${tableNum * count}`);
+document.write("<br />");
+count++; 
+ document.write(`${tableNum} * ${count} = ${tableNum * count}`);
+document.write("<br />");
+count++; 
