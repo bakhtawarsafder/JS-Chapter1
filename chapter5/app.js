0cfc1e3b2b73bdc9f5f58caf8cc4374b67d65189
mahnoorshaikh608 <mahnoorshaikh608@gmail.com>
2026-03-08 14:07:39 +0500

    done

--- chapter5/app.js
+++ chapter5/app.js
@@ -1,31 +1,97 @@
 // question1
 
-let num1 = +prompt( "Enter your First Number");
-let num2 = +prompt ( "Enter your Second Number");
+let num8 = +prompt( "Enter your First Number");
+let num9 = +prompt ( "Enter your Second Number");
 
 let sum = num1 + num2;
-document.write(`Sum of ${num1} and ${num2} is ${sum}`);
+document.write(`Sum of ${num8} and ${num9} is ${sum}`);
 document.write("<br />");
 
 
 // question2
-// b. Show the value of variable in your browser like “Value
-// after variable declaration is: ??”.
-let num = ("three")
 
-document.write(`Value after variable declaration is ${num}}`);
-document.write("<br />");
+
+
+
+
+
 
 // question3
+// b. Show the value of variable in your browser like “Value
+// after variable declaration is: ??”.
+let num7 = ("three")
+
+document.write(`Value after variable declaration is ${num7}`);
+document.write("<br />");
 
 
+
+// d. Show the value of variable in your browser like “Initial 
+// value: 5”. 
+
+let num3 = ("5")
+
+document.write(`Initial value: ${num3}`);
+document.write("<br />");
+
+
+// e. Increment the variable. 
+
+
+
+
+
+// f. Show the value of variable in your browser like “Value 
+// after increment is: 6”. 
+
+let num4 = ("6")
+
+document.write(`value after increment is : ${num4}`);
+document.write("<br />");
+
+
+
+
+// g. Add 7 to the variable.
+ let num5 = ("5")
+++num;
+document.write
+
+
+
+// j. Show the value of variable in your browser like “Value 
+// after decrement is: 12”.
+let num6 = ("12")
+
+document.write(`value after decrement is : ${num6}`);
+document.write("<br />");
+
+
+
+// h. Show the value of variable in your browser like “Value 
+// after addition is: 13”.
+ var num= 6;
+num = num + 7;
+
+document.write("Value after addition is: " + num);
+document.write("<br />");
+
+// l. Output : “The remainder is : 0”.
+
+var num1 = 10;
+var num2 = 5;
+
+var remainder = num1 % num2;
+
+document.write("The remainder is : " + remainder);
+document.write("<br />");
 
 
 // question4
 let cost = ("600")
 let total =("5")
  let multiply =cost * total 
-document.write(`Total cost to buy ${total} tickets to a movie is ${multiply}`);
+document.write(`Total cost to buy ${total} tickets to a movie is ${multiply}PKR`);
 document.write("<br />");
 
 
@@ -71,3 +137,14 @@ count++;
  document.write(`${tableNum} * ${count} = ${tableNum * count}`);
 document.write("<br />");
 count++; 
+
+
+
+// let num6= +prompt("enter your number" ) 
+// if(num%2===0){
+//     alert (`${num6} is an even num`)
+// }
+// else{
+//  alert (`${num6} is an odd num`)
+// }
+
